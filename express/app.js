f94e7fc7fa0acdd7cbd02e9be49407bace094311
MyoMyintAung <myomyintaung064@gmail.com>
2022-09-21 09:49:01 +0630

    filter the http request methods

--- express/app.js
+++ express/app.js
@@ -1,22 +1,24 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //  return middleware function and call next()
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/add-product' ,(req, res)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="name"/><button>Add Product</button></form>');
-})
+app.use("/add-product", (req, res) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="name"/><button>Add Product</button></form>'
+  );
+});
 
-app.use('/product', (req, res)=>{
-    console.log(req.body.name);
-    res.redirect('/');
-})
+app.post("/product", (req, res) => {
+  console.log(req.body.name);
+  res.redirect("/");
+});
 
-app.use('/' ,(req, res)=>{
-    res.send('<h1>Hello from  Express.js</h1>');
-})
+app.use("/", (req, res) => {
+  res.send("<h1>Hello from  Express.js</h1>");
+});
 
-app.listen(3000)
+app.listen(3000);

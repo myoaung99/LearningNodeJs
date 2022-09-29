18a53197b5161a44795aa934828af230ee4c9fb4
MyoMyintAung <myomyintaung064@gmail.com>
2022-09-29 08:46:27 +0630

    passing data via post method instead of params

--- dynamic_route/controllers/shop.js
+++ dynamic_route/controllers/shop.js
@@ -16,7 +16,7 @@ exports.getProduct = (req, res, next) => {
     res.render("shop/product-detail", {
       prod: product,
       pageTitle: product.title,
-      path: null,
+      path: "/products",
     })
   );
 };
@@ -36,6 +36,12 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {

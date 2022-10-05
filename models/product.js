d5e850050e60fd8667f64ad8faca441033153b99
MyoMyintAung <myomyintaung064@gmail.com>
2022-10-05 18:02:18 +0630

    fetch single product from mongodb

--- models/product.js
+++ models/product.js
@@ -1,34 +1,46 @@
-const {getDb} = require("./../util/database");
+const { ObjectId } = require("mongodb");
+const { getDb } = require("./../util/database");
 
 module.exports = class Product {
-    constructor(title, imageUrl, description, price) {
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        const db = getDb();
-        return db
-            .collection("products")
-            .insertOne(this)
-            .then((result) =>
-                console.log("saved product's id: ", result.insertedId.toString())
-            )
-            .catch((err) => console.log(err));
-    }
+  save() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .insertOne(this)
+      .then((result) =>
+        console.log("saved product's id: ", result.insertedId.toString())
+      )
+      .catch((err) => console.log(err));
+  }
 
-    static fetchAll() {
-        const db = getDb();
-        return db
-            .collection("products")
-            .find()
-            .toArray()
-            .then((result) => {
-                console.log(result);
-                return result;
-            })
-            .catch((err) => console.log(err));
-    }
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection("products")
+      .find()
+      .toArray()
+      .then((result) => {
+        console.log(result);
+        return result;
+      })
+      .catch((err) => console.log(err));
+  }
+
+  static fetchById(id) {
+    const db = getDb();
+    return db
+      .collection("products")
+      .findOne({ _id: new ObjectId(id) })
+      .then((result) => {
+        return result;
+      })
+      .catch((err) => console.log(err));
+  }
 };
